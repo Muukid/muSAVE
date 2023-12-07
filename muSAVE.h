767c7284440c15645b67c393f45cc027da938f96
Hum <sc.mukid@gmail.com>
2023-12-07 06:16:26 -0800

    Add MUDEF to 'mu_spirv_binary_to_assembly'

--- muSAVE.h
+++ muSAVE.h
@@ -331,7 +331,7 @@ MUDEF muSPIRVInstruction mu_global_SPIRV_instructions[MU_SPIRV_INSTRUCTION_COUNT
 
 /* functions */
 
-muString mu_spirv_binary_to_assembly(muResult* result, const char* binary, size_m len);
+MUDEF muString mu_spirv_binary_to_assembly(muResult* result, const char* binary, size_m len);
 
 #ifdef __cplusplus
 }
@@ -3830,7 +3830,7 @@ muString muSAVE_insert_instruction(muString str, size_m i, size_m j, uint16_m st
 const char* mu_global_spirv_binary_to_assembly_error_msg = "[MUSAVE] Failed to convert SPIR-V binary to assembly; SPIR-V binary is incorrect/corrupted.\n";
 #define MU_SPIRV_BASSERT(c) {if(!(c)){mu_printf("%s",mu_global_spirv_binary_to_assembly_error_msg);str=mu_string_destroy(str);if(result!=MU_NULL_PTR){*result=MU_FAILURE;}return empty_str;}}
 
-muString mu_spirv_binary_to_assembly(muResult* result, const char* binary, size_m len) {
+MUDEF muString mu_spirv_binary_to_assembly(muResult* result, const char* binary, size_m len) {
 	muString empty_str = {0};
 	empty_str.s = MU_NULL_PTR;
 	empty_str.ws = MU_NULL_PTR;
